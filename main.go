082631975cb67c0c867e4ba7d5db06162fd4aaf2
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-05-30 15:14:52 +0200

    feat: basic HTTP request and response

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, morph!\n")
+}
 
 func main() {
 	fmt.Println("hello, morph")
+	http.HandleFunc("/test", helloWorldHandler)
+	http.ListenAndServe(":8080", nil)
 }
