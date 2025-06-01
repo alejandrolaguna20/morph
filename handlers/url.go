538ba6c764f1f32f242f9e898182af7fef9f0895
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-06-01 18:37:44 +0200

    feat: load env

--- handlers/url.go
+++ handlers/url.go
@@ -14,6 +14,7 @@ func PostShortenUrlHandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusBadRequest)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
--- main.go
+++ main.go
@@ -3,16 +3,68 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/alejandrolaguna20/morph/handlers"
+	dotenv "github.com/joho/godotenv"
 )
+
+type Env struct {
+	DatabaseName     string
+	DatabasePassword string
+	DatabaseUser     string
+	DatabasePort     int
+	DatabaseHost     string
+	ServerPort       int
+}
 
 func handlersSetup() {
 	http.HandleFunc("/hello", handlers.HelloWorldHandler)
 	http.HandleFunc("/url", handlers.PostShortenUrlHandler)
 }
 
+func getEnvOrFatal(s string) string {
+	value := os.Getenv(s)
+	if value == "" {
+		log.Fatalf("Environment variable [%s] not set", s)
+	}
+	return value
+}
+
+func loadEnv() Env {
+	env := Env{}
+	err := dotenv.Load()
+
+	if err != nil {
+		log.Fatal(".env file not loaded, aborting")
+	}
+
+	env.DatabaseHost = getEnvOrFatal("DB_HOST")
+	env.DatabasePassword = getEnvOrFatal("DB_PASSWORD")
+	env.DatabaseName = getEnvOrFatal("DB_NAME")
+	env.DatabaseUser = getEnvOrFatal("DB_USER")
+
+	port, err := strconv.Atoi(getEnvOrFatal("DB_PORT"))
+	if err != nil {
+		log.Fatal("Database port missing")
+	} else {
+		env.DatabasePort = port
+	}
+
+	port, err = strconv.Atoi(getEnvOrFatal("PORT"))
+	if err != nil {
+		log.Fatal("Server port missing")
+	} else {
+		env.ServerPort = port
+	}
+
+	return env
+}
+
 func main() {
+	env := loadEnv()
+	portString := ":" + strconv.Itoa(env.ServerPort)
 	handlersSetup()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(portString, nil))
 }
