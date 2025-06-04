8319fd14312bccd3a77dc81e9234213ed4e479b0
Alejandro Laguna <alejandro.laguna@epitech.eu>
2025-06-04 14:15:19 +0200

    chore: setup frontend

---
+++ web/src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1 class="text-red-500"> hello, morph </h1>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
